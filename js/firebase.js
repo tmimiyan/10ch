042d82ccad72176ffdb57f4d7251bee3ce60f005
mimiyan <79374959+tmimiyan@users.noreply.github.com>
2026-07-27 22:00:08 +0900

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA41mpWT_KWOlC8V2JiR_pf2gJT063sK1k",
@@ -17,3 +18,4 @@ export const isFirebaseConfigured = !Object.values(firebaseConfig).some((value) 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
