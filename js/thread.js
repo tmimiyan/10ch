87ab2b46f103feee1b4c98b13276238d72392c18
mimiyan <79374959+tmimiyan@users.noreply.github.com>
2026-08-24 00:13:50 +0900

    Update import statement for user.js with version query

--- js/thread.js
+++ js/thread.js
@@ -5,7 +5,7 @@ import { auth, db, isFirebaseConfigured } from "./firebase.js";
 import { removePostImages, uploadPostImages, validateImages } from "./media.js";
 import { $, displayName, firebaseMessage, formatDate, isAdmin, showToast } from "./util.js";
 import { initTheme } from "./theme.js";
-import { getFirstLoginAt } from "./user.js";
+import { getFirstLoginAt } from "./user.js?v=20260824-2";
 
 initTheme();
 const id = new URLSearchParams(location.search).get("id");
